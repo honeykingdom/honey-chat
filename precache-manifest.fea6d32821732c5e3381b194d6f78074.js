b885fddcb844956e1010e3e91f4e533e062b68dd
DmitryScaletta <dmitryscaletta@mail.ru>
2019-10-27 10:20:25 +0300

    Updates

---
+++ precache-manifest.fea6d32821732c5e3381b194d6f78074.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdc3fd3aa92899dcdc14001ebf362a1",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "27d7b018aab6055108e5",
+    "url": "/chat/static/js/2.e3050ef8.chunk.js"
+  },
+  {
+    "revision": "bda44a504c8bfbda54ab",
+    "url": "/chat/static/js/main.e4a6c226.chunk.js"
+  },
+  {
+    "revision": "b1344628fe4d60e139b7",
+    "url": "/chat/static/js/runtime-main.a7124b14.js"
+  }
+]);
