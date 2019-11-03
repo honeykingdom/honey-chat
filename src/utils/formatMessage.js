655f394bdf77503e69d61fb2e29bda425d80200b
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-03 12:37:38 +0300

    fix: add formatLinks function

--- src/utils/formatMessage.js
+++ src/utils/formatMessage.js
@@ -1,3 +1,21 @@
+// import urlRegex from 'url-regex';
+
+// const formatLinks = (text) => {
+//   let arr;
+//   const regexp = urlRegex({ strict: false });
+
+//   while ((arr = regexp.exec(text)) !== null) {
+//     return text.replace(
+//       arr[0],
+//       `<a href="${arr[0]}" target="_blank" rel="noopener noreferrer">${
+//         arr[0]
+//       }</a>`,
+//     );
+//   }
+
+//   return text;
+// };
+
 const formatEmotes = (text = '', emotes) => {
   let splitText = text.split('');
 
