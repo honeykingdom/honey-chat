415d39798e66b91320fdc6af333b54622ca90deb
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-03 13:07:09 +0300

    feat: add .env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 export const TWITCH_API_AUTH_BASE = 'https://id.twitch.tv/oauth2/authorize';
 export const TWITCH_API_BASE = 'https://api.twitch.tv/helix';
 
-export const TWITCH_API_CLIENT_ID = '4e66w1po1tzf645r9vutn9qus05vg9x';
-export const TWITCH_API_REDIRECT_URI = 'http://localhost:3000/auth/callback';
+export const TWITCH_API_CLIENT_ID = process.env.REACT_APP_TWITCH_API_CLIENT_ID;
+export const TWITCH_API_REDIRECT_URI =
+  process.env.REACT_APP_TWITCH_API_REDIRECT_URI;
 
 export const MAIN_CHANNEL_NAME = 'nasvay_s';
