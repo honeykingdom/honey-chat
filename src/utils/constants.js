6f625c2e9a16b34cc795e1fed52d8116196ffa66
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-03 12:38:09 +0300

    fix: add twitch api constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
-export const TWITCH_CLIENT_ID = '4e66w1po1tzf645r9vutn9qus05vg9x';
-export const TWITCH_REDIRECT_URI = 'http://localhost:3000/';
+export const TWITCH_API_AUTH_BASE = 'https://id.twitch.tv/oauth2/authorize';
+export const TWITCH_API_BASE = 'https://api.twitch.tv/helix';
 
-export const MAIN_CHANNEL_NAME = 'nasvay_s';
+export const TWITCH_API_CLIENT_ID = '4e66w1po1tzf645r9vutn9qus05vg9x';
+export const TWITCH_API_REDIRECT_URI = 'http://localhost:3000/auth/callback';
+
+export const MAIN_CHANNEL_NAME = 'dmitryscaletta';
