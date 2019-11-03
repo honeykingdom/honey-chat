f72df6efa43ef21c931f7a1fc80a5bb4973a7dcd
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-03 12:43:35 +0300

    fix: change main channel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,4 +4,4 @@ export const TWITCH_API_BASE = 'https://api.twitch.tv/helix';
 export const TWITCH_API_CLIENT_ID = '4e66w1po1tzf645r9vutn9qus05vg9x';
 export const TWITCH_API_REDIRECT_URI = 'http://localhost:3000/auth/callback';
 
-export const MAIN_CHANNEL_NAME = 'dmitryscaletta';
+export const MAIN_CHANNEL_NAME = 'nasvay_s';
