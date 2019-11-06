66f6481611f56d2b3a6ab79187122d3d4e4a3299
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-06 10:36:11 +0300

    feat: add new api requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,48 @@
-import { TWITCH_API_BASE } from './constants';
+export const TWITCH_API_HELIX = 'https://api.twitch.tv/helix';
+export const TWITCH_API_KRAKEN = 'https://api.twitch.tv/kraken';
 
-export const getAuthorizationHeader = () => ({
+// TODO: throw an error if fetch completed with not 2** code
+
+const helixHeaders = {
   Authorization: `Bearer ${localStorage.accessToken}`,
-});
+};
 
-export const apiRequest = (url) =>
-  fetch(url, {
-    headers: getAuthorizationHeader(),
+const krakenHeaders = {
+  Accept: 'application/vnd.twitchtv.v5+json',
+  'Client-ID': process.env.REACT_APP_TWITCH_API_CLIENT_ID,
+  Authorization: `OAuth ${localStorage.accessToken}`,
+};
+
+const apiRequestHelix = (url) =>
+  fetch(`${TWITCH_API_HELIX}${url}`, {
+    headers: helixHeaders,
   }).then((response) => response.json());
 
-/*
-{
-  "data": [{
-    "id": "44322889",
-    "login": "dallas",
-    "display_name": "dallas",
-    "type": "staff",
-    "broadcaster_type": "",
-    "description": "Just a gamer playing games and chatting. :)",
-    "profile_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/dallas-profile_image-1a2c906ee2c35f12-300x300.png",
-    "offline_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/dallas-channel_offline_image-1a2c906ee2c35f12-1920x1080.png",
-    "view_count": 191836881,
-    "email": "login@provider.com"
-  }]
-}
-*/
-export const fetchUser = (id) =>
-  apiRequest(`${TWITCH_API_BASE}/users?id=${id}`);
+const apiRequestKraken = (url) =>
+  fetch(`${TWITCH_API_KRAKEN}${url}`, {
+    headers: krakenHeaders,
+  }).then((response) => response.json());
+
+const apiRequest = (url) => fetch(url).then((response) => response.json());
+
+export const fetchUser = (id) => apiRequestHelix(`/users?id=${id}`);
+
+export const fetchTwitchEmotesBySets = (userId) =>
+  apiRequestKraken(`/users/${userId}/emotes`);
+
+export const fetchBttvGlobalEmotes = () =>
+  apiRequest('https://api.betterttv.net/3/cached/emotes/global');
+export const fetchBttvChannelEmotes = (channelId) =>
+  apiRequest(`https://api.betterttv.net/3/cached/users/twitch/${channelId}`);
+
+export const fetchFfzGlobalEmotes = () =>
+  apiRequest('https://api.frankerfacez.com/v1/set/global');
+export const fetchFfzChannelEmotes = (channelId) =>
+  apiRequest(`https://api.frankerfacez.com/v1/room/id/${channelId}`);
+
+// apiRequest(`https://api.betterttv.net/3/cached/frankerfacez/users/twitch/${channelId}`);
 
 export const fetchRecentMessages = (channel) =>
-  fetch(
+  apiRequest(
     `https://recent-messages.robotty.de/api/v2/recent-messages/${channel}?clearchatToNotice=true`,
-  ).then((response) => response.json());
+  );
