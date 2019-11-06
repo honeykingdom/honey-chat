4ab9a552380e9c22e5e95e6ef5fc0590c2cd4812
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-06 10:38:54 +0300

    fix: update messages reducer

--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -1,11 +1,12 @@
-import {
-  createAction,
-  createActions,
-  handleActions,
-  combineActions,
-} from 'redux-actions';
+import { createActions, handleActions, combineActions } from 'redux-actions';
+import { pathOr } from 'ramda';
 import { parse } from 'tekko';
 
+import {
+  twitchEmotesSelector,
+  bttvEmotesSelector,
+  ffzEmotesSelector,
+} from './emotes/selectors';
 import { fetchRecentMessages as apiFetchRecentMessages } from '../utils/api';
 import { CHANNEL_MESSAGES_LIMIT } from '../utils/constants';
 import {
@@ -13,82 +14,163 @@ import {
   normalizeActionMessage,
   parseMessageTags,
 } from '../utils/twitchChat';
-// import formatMessage from '../utils/formatMessage';
+import formatMessage from '../utils/formatMessage';
 
 const defaultState = {
-  isLoading: false,
-  items: [],
+  // [channel]: {
+  //   history: {
+  //     isLoading: true,
+  //     isLoaded: false,
+  //     isError: false,
+  //     error: null,
+  //   },
+  //   items: [],
+  // },
 };
 
 const sliceMessages = (items) => {
-  const itemsDiff = items.length - CHANNEL_MESSAGES_LIMIT;
-
-  return itemsDiff > 0 ? items.slice(itemsDiff) : items;
+  const diff = items.length - CHANNEL_MESSAGES_LIMIT;
+  return diff > 0 ? items.slice(diff) : items;
 };
 
-export const { addMessage, addMessages } = createActions({
-  ADD_MESSAGE: (message) => [message],
-  ADD_MESSAGES: (messages) => messages,
+const getEmotes = (state) => ({
+  twitch: twitchEmotesSelector(state),
+  bttv: bttvEmotesSelector(state),
+  ffz: ffzEmotesSelector(state),
 });
 
-const fetchRecentMessagesRequest = createAction(
+const {
+  addMessages: addMessagesRequest,
+  fetchRecentMessagesRequest,
+  fetchRecentMessagesSuccess,
+  fetchRecentMessagesFailure,
+} = createActions(
+  'ADD_MESSAGES',
   'FETCH_RECENT_MESSAGES_REQUEST',
-);
-const fetchRecentMessagesSuccess = createAction(
   'FETCH_RECENT_MESSAGES_SUCCESS',
-);
-const fetchRecentMessagesFailure = createAction(
   'FETCH_RECENT_MESSAGES_FAILURE',
 );
 
-export const fetchRecentMessages = (dispatch) => async (channel) => {
-  dispatch(fetchRecentMessagesRequest());
-
-  const { messages } = await apiFetchRecentMessages(channel);
-
-  const normalizedMessages = messages
+const normalizeRecentMessages = (messages) =>
+  messages
     .map((m) => parse(m))
     .filter((m) => m.command === 'PRIVMSG')
-    .map(({ tags, params: [, message] }) => {
+    .map(({ tags, params: [channel, message], prefix: { user } }) => {
       const isAction = getIsAction(message);
       return {
-        tags: parseMessageTags(tags),
         message: isAction ? normalizeActionMessage(message) : message,
+        tags: parseMessageTags(tags),
+        user,
+        channel: channel.slice(1),
         isAction,
         isHistory: true,
       };
     });
 
-  dispatch(fetchRecentMessagesSuccess(normalizedMessages));
+export const fetchRecentMessages = (channel) => async (dispatch, getState) => {
+  dispatch(fetchRecentMessagesRequest({ channel }));
+  try {
+    const response = await apiFetchRecentMessages(channel);
+    const data = {
+      channel,
+      items: formatMessage(
+        normalizeRecentMessages(response.messages),
+        getEmotes(getState()),
+      ),
+    };
+
+    dispatch(fetchRecentMessagesSuccess(data));
+  } catch (error) {
+    dispatch(fetchRecentMessagesFailure({ channel, error }));
+  }
 };
 
-const handleFetchRecentMessagesRequest = (state) => ({
-  ...state,
-  isLoading: true,
-});
-const handleFetchRecentMessagesSuccess = (state, { payload }) => ({
-  ...state,
-  isLoading: false,
-  items: sliceMessages(payload),
-});
-const handleFetchRecentMessagesFailure = (state, { payload }) => ({
-  ...state,
-  isLoading: false,
-  isError: true,
-  error: payload,
-});
+const handleFetchRecentMessages = (state, { type, payload }) => {
+  const { channel } = payload;
 
-const handleAddMessages = (state, { payload }) => ({
-  ...state,
-  items: sliceMessages([...state.items, ...payload]),
-});
+  if (type === fetchRecentMessagesRequest.toString()) {
+    const oldItems = pathOr([], [channel, 'items'], state);
+    return {
+      ...state,
+      [channel]: {
+        ...state[channel],
+        history: {
+          isLoading: true,
+          isLoaded: false,
+          isError: false,
+          error: null,
+        },
+        items: oldItems,
+      },
+    };
+  }
+
+  if (type === fetchRecentMessagesSuccess.toString()) {
+    return {
+      ...state,
+      [channel]: {
+        ...state[channel],
+        history: {
+          isLoading: false,
+          isLoaded: true,
+          isError: false,
+          error: null,
+        },
+        items: sliceMessages([...payload.items, ...state[channel].items]),
+      },
+    };
+  }
+
+  if (type === fetchRecentMessagesFailure.toString()) {
+    return {
+      ...state,
+      [channel]: {
+        ...state[channel],
+        history: {
+          isLoading: false,
+          isLoaded: false,
+          isError: true,
+          error: payload.error,
+        },
+      },
+    };
+  }
+
+  return state;
+};
+
+export const addMessages = (payload) => (dispatch, getState) => {
+  const newItems = formatMessage(payload.items, getEmotes(getState()));
+
+  dispatch(
+    addMessagesRequest({
+      ...payload,
+      items: newItems,
+    }),
+  );
+};
+
+const handleAddMessages = (state, { payload }) => {
+  const { channel } = payload;
+  const oldItems = pathOr([], [channel, 'items'], state);
+
+  return {
+    ...state,
+    [channel]: {
+      ...state[channel],
+      items: sliceMessages([...oldItems, ...payload.items]),
+    },
+  };
+};
 
 const reducer = handleActions(
   {
-    [combineActions(addMessage, addMessages)]: handleAddMessages,
-    [fetchRecentMessagesRequest]: handleFetchRecentMessagesRequest,
-    [fetchRecentMessagesSuccess]: handleFetchRecentMessagesSuccess,
-    [fetchRecentMessagesFailure]: handleFetchRecentMessagesFailure,
+    [addMessagesRequest]: handleAddMessages,
+    [combineActions(
+      fetchRecentMessagesRequest,
+      fetchRecentMessagesSuccess,
+      fetchRecentMessagesFailure,
+    )]: handleFetchRecentMessages,
   },
   defaultState,
 );
