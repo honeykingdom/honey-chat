9458e125e967c3a6b1e7de172710c1c5dad2394b
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-06 10:36:32 +0300

    feat: add chat reducer

---
+++ src/reducers/chat.js
@@ -0,0 +1,75 @@
+import { createActions, handleActions } from 'redux-actions';
+import { omit } from 'ramda';
+
+const defaultState = {
+  currentChannel: null,
+  isConnected: false,
+  globalState: {},
+  channels: {
+    // [channel]: {
+    //   userState: {},
+    //   roomState: {},
+    // }
+  },
+};
+
+export const {
+  setCurrentChannel,
+  setIsConnected,
+  updateGlobalUserState,
+  updateUserState,
+  updateRoomState,
+  removeChannel,
+} = createActions(
+  'SET_CURRENT_CHANNEL',
+  'SET_IS_CONNECTED',
+  'UPDATE_GLOBAL_USER_STATE',
+  'UPDATE_USER_STATE',
+  'UPDATE_ROOM_STATE',
+  'REMOVE_CHANNEL',
+);
+
+const handleSetCurrentChannel = (state, { payload }) => ({
+  ...state,
+  currentChannel: payload,
+});
+const handleSetIsConnected = (state, { payload }) => ({
+  ...state,
+  isConnected: payload,
+});
+const handleUpdateGlobalUserState = (state, { payload: { tags } }) => ({
+  ...state,
+  globalState: { ...state.globalState, ...tags },
+});
+const handleUpdateUserState = (state, { payload: { channel, tags } }) => ({
+  ...state,
+  channels: {
+    ...state.channels,
+    [channel]: { ...state.channels[channel], userState: tags },
+  },
+});
+const handleUpdateRoomState = (state, { payload: { channel, tags } }) => ({
+  ...state,
+  channels: {
+    ...state.channels,
+    [channel]: { ...state.channels[channel], roomState: tags },
+  },
+});
+const handleRemoveChannel = (state, { payload: channel }) => ({
+  ...state,
+  channels: omit([channel], state.channels),
+});
+
+const reducer = handleActions(
+  {
+    [setCurrentChannel]: handleSetCurrentChannel,
+    [setIsConnected]: handleSetIsConnected,
+    [updateGlobalUserState]: handleUpdateGlobalUserState,
+    [updateUserState]: handleUpdateUserState,
+    [updateRoomState]: handleUpdateRoomState,
+    [removeChannel]: handleRemoveChannel,
+  },
+  defaultState,
+);
+
+export default reducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth';
+import chat from './chat';
+import emotes from './emotes/index';
 import messages from './messages';
 
 export default combineReducers({
   auth,
+  chat,
+  emotes,
   messages,
 });
