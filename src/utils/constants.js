ff3dbb32de7d8978541a5820db89dc787a08f2a6
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-06 10:37:46 +0300

    fix: remove unused constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,7 @@
 export const TWITCH_API_AUTH_BASE = 'https://id.twitch.tv/oauth2/authorize';
-export const TWITCH_API_BASE = 'https://api.twitch.tv/helix';
 
 export const TWITCH_API_CLIENT_ID = process.env.REACT_APP_TWITCH_API_CLIENT_ID;
 export const TWITCH_API_REDIRECT_URI =
   process.env.REACT_APP_TWITCH_API_REDIRECT_URI;
-
-export const MAIN_CHANNEL_NAME = 'nasvay_s';
 
 export const CHANNEL_MESSAGES_LIMIT = 500;
