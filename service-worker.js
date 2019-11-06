18a178b895590caf448004c6fcc3ead6f202cc76
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-06 15:27:25 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chat/precache-manifest.e5173f5d5cae184072f98f7019221f2a.js"
+  "/chat/precache-manifest.0dbaa05db23d885495150461131f920d.js"
 );
 
 self.addEventListener('message', (event) => {
