e3787e39b02fa224d9fca339d0f5dc2fce3740d6
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-07 11:58:05 +0300

    fix: add store variable to the test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from './store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App store={store} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
