2b1b89ddd64140b813effb19d1adaeb60c4dbc18
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-07 12:03:12 +0300

    feat: render different items inside a message

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import pt from 'prop-types';
 import styled from 'styled-components';
 
+import {
+  twitchEmoteType,
+  bttvEmoteType,
+  ffzEmoteType,
+  emojiType,
+  mentionType,
+  linkType,
+} from '../utils/formatMessage';
+
 const ChatMessageRoot = styled.div`
   padding: 5px 20px;
   color: ${(p) => (p.isAction ? p.color : '#fff')};
@@ -12,35 +21,41 @@ const ChatMessageRoot = styled.div`
   &:nth-child(even) {
     background-color: #1f1925;
   }
-
-  .chat-image {
-    display: inline-block;
-    margin: -5px 0;
-    vertical-align: middle;
-  }
-
-  a {
-    color: #bf94ff;
-    text-decoration: none;
-
-    &:focus,
-    &:hover {
-      color: #a970ff;
-      text-decoration: underline;
-    }
-
-    &:visited {
-      color: #a970ff;
-    }
-  }
 `;
 const Name = styled.span`
   font-weight: bold;
   color: ${(p) => p.color};
 `;
-const Text = styled.span``;
+const Emote = styled.img`
+  display: inline-block;
+  margin: -5px 0;
+  vertical-align: middle;
+`;
+const Emoji = styled.img`
+  display: inline-block;
+  margin-top: -5px;
+  margin-bottom: -4px;
+  width: 20px;
+  height: auto;
+  vertical-align: middle;
+`;
+const Mention = styled.span``;
+const Link = styled.a.attrs({ rel: 'noreferrer noopener', target: '_blank' })`
+  color: #bf94ff;
+  text-decoration: none;
 
-const tagsType = pt.shape({
+  &:focus,
+  &:hover {
+    color: #a970ff;
+    text-decoration: underline;
+  }
+
+  &:visited {
+    color: #a970ff;
+  }
+`;
+
+export const tagsType = pt.shape({
   badgeInfo: pt.shape({
     subscriber: pt.number,
   }),
@@ -56,15 +71,51 @@ const tagsType = pt.shape({
   userId: pt.string,
 });
 
+const renderMessageArray = (item, key) => {
+  if (typeof item !== 'object') return item;
+
+  if (
+    item.type === 'twitch-emote' ||
+    item.type === 'bttv-emote' ||
+    item.type === 'ffz-emote'
+  ) {
+    return (
+      <Emote key={key} src={item.src} srcSet={item.srcSet} alt={item.alt} />
+    );
+  }
+
+  if (item.type === 'emoji') {
+    return (
+      <Emoji key={key} src={item.src} srcSet={item.srcSet} alt={item.alt} />
+    );
+  }
+
+  if (item.type === 'mention') {
+    return <Mention key={key}>{item.text}</Mention>;
+  }
+
+  if (item.type === 'link') {
+    // console.log(item);
+    return (
+      <Link key={key} href={item.href}>
+        {item.text}
+      </Link>
+    );
+  }
+
+  return null;
+};
+
 const ChatMessage = ({
-  message,
+  // message,
+  messageArray,
   tags: { color, displayName },
   isHistory,
   isAction,
 }) => (
   <ChatMessageRoot isHistory={isHistory} isAction={isAction} color={color}>
     <Name color={color}>{displayName}</Name>
-    {!isAction && ':'} <Text dangerouslySetInnerHTML={{ __html: message }} />
+    {!isAction && ':'} {messageArray.map(renderMessageArray)}
   </ChatMessageRoot>
 );
 
@@ -74,8 +125,21 @@ ChatMessage.defaultProps = {
 };
 
 ChatMessage.propTypes = {
+  // message: pt.string.isRequired,
+  messageArray: pt.arrayOf(
+    pt.oneOfType([
+      pt.string,
+      pt.shape({}),
+      twitchEmoteType,
+      bttvEmoteType,
+      ffzEmoteType,
+      emojiType,
+      mentionType,
+      // TODO: fix warning with link type
+      linkType,
+    ]),
+  ).isRequired,
   tags: tagsType.isRequired,
-  message: pt.string.isRequired,
   isHistory: pt.bool,
   isAction: pt.bool,
 };
