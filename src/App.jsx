7e87245f82a902db3e3b1f63df3f26e33d8b7ec6
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-08 17:25:25 +0300

    fix: use isAuthRedirect function

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import pt from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import isAuthRedirect from './utils/isAuthRedirect';
 import Home from './components/Home';
 import Auth from './components/Auth';
 import AuthCallback from './components/AuthCallback';
 import GlobalStyles from './styles';
 
-const getHomeComponent = () => {
-  const isAuthRedirect = window.location.hash.startsWith('#access_token=');
-  return isAuthRedirect ? AuthCallback : Home;
-};
+const getHomeComponent = () =>
+  isAuthRedirect(window.location.hash) ? AuthCallback : Home;
 
 const App = ({ store }) => (
   <>
