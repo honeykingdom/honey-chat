bbb193cfe0d0de64be891c57413a2e179c8bc45e
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-09 20:23:43 +0300

    fix: don't use redex devtools in production

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,16 @@ import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
-const store = createStore(
-  rootReducer,
-  {},
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (noop) => noop,
-  ),
-);
+const enhancer =
+  process.env.NODE_ENV === 'production'
+    ? applyMiddleware(thunk)
+    : compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__
+          ? window.__REDUX_DEVTOOLS_EXTENSION__()
+          : (noop) => noop,
+      );
+
+const store = createStore(rootReducer, {}, enhancer);
 
 export default store;
