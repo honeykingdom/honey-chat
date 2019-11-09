0c3a22d532b28c6c8377122f9b8ec3c2786a918e
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-09 10:51:56 +0300

    feat: add badges reducer

---
+++ src/reducers/badges.js
@@ -0,0 +1,194 @@
+import { createActions, handleActions, combineActions } from 'redux-actions';
+import { pathOr } from 'ramda';
+
+import { currentChannelSelector } from './chat';
+
+import {
+  fetchGlobalBadges as apiFetchGlobalBadges,
+  fetchChannelBadges as apiFetchChannelBadges,
+} from '../utils/api';
+
+const defaultState = {
+  global: {
+    isLoading: false,
+    isLoaded: false,
+    isError: false,
+    error: null,
+    items: {},
+  },
+  channels: {
+    // [channel]: {
+    //   isLoading: false,
+    //   isLoaded: false,
+    //   isError: false,
+    //   error: null,
+    //   items: {},
+    // },
+  },
+};
+
+export const globalBadgesSelector = (state) => state.badges.global.items;
+export const channelBadgesSelector = (state) =>
+  pathOr({}, ['badges', 'channels', currentChannelSelector(state), 'items']);
+
+const {
+  fetchGlobalBadgesRequest,
+  fetchGlobalBadgesSuccess,
+  fetchGlobalBadgesFailure,
+  fetchChannelBadgesRequest,
+  fetchChannelBadgesSuccess,
+  fetchChannelBadgesFailure,
+} = createActions(
+  'FETCH_GLOBAL_BADGES_REQUEST',
+  'FETCH_GLOBAL_BADGES_SUCCESS',
+  'FETCH_GLOBAL_BADGES_FAILURE',
+  'FETCH_CHANNEL_BADGES_REQUEST',
+  'FETCH_CHANNEL_BADGES_SUCCESS',
+  'FETCH_CHANNEL_BADGES_FAILURE',
+);
+
+const parseBadges = pathOr({}, ['badge_sets']);
+
+export const fetchGlobalBadges = () => async (dispatch) => {
+  dispatch(fetchGlobalBadgesRequest());
+
+  try {
+    const response = await apiFetchGlobalBadges();
+    const data = { items: parseBadges(response) };
+
+    dispatch(fetchGlobalBadgesSuccess(data));
+  } catch (error) {
+    dispatch(fetchGlobalBadgesFailure({ error }));
+  }
+};
+
+export const fetchChannelBadges = (channelId, channel) => async (dispatch) => {
+  dispatch(fetchChannelBadgesRequest({ channel }));
+
+  try {
+    const response = await apiFetchChannelBadges(channelId);
+    const data = {
+      channel,
+      items: parseBadges(response),
+    };
+
+    dispatch(fetchChannelBadgesSuccess(data));
+  } catch (error) {
+    dispatch(fetchChannelBadgesFailure({ channel, error }));
+  }
+};
+
+const handleFetchFfzGlobalEmotes = (state, { type, payload }) => {
+  if (type === fetchGlobalBadgesRequest.toString()) {
+    return {
+      ...state,
+      global: {
+        ...state.global,
+        isLoading: true,
+        isLoaded: false,
+        isError: false,
+        error: null,
+      },
+    };
+  }
+
+  if (type === fetchGlobalBadgesSuccess.toString()) {
+    return {
+      ...state,
+      global: {
+        ...state.global,
+        isLoading: false,
+        isLoaded: true,
+        isError: false,
+        error: null,
+        ...payload,
+      },
+    };
+  }
+
+  if (type === fetchGlobalBadgesFailure.toString()) {
+    return {
+      ...state,
+      global: {
+        ...state.global,
+        isLoading: false,
+        isLoaded: false,
+        isError: true,
+        ...payload,
+      },
+    };
+  }
+
+  return state;
+};
+
+const handleFetchFfzChannelEmotes = (state, { type, payload }) => {
+  const { channel } = payload;
+
+  if (type === fetchChannelBadgesRequest.toString()) {
+    return {
+      ...state,
+      channels: {
+        ...state.channels,
+        [channel]: {
+          isLoading: true,
+          isLoaded: false,
+          isError: false,
+          error: null,
+        },
+      },
+    };
+  }
+
+  if (type === fetchChannelBadgesSuccess.toString()) {
+    return {
+      ...state,
+      channels: {
+        ...state.channels,
+        [channel]: {
+          isLoading: false,
+          isLoaded: true,
+          isError: false,
+          error: null,
+          items: payload.items,
+        },
+      },
+    };
+  }
+
+  if (type === fetchChannelBadgesFailure.toString()) {
+    return {
+      ...state,
+      channels: {
+        ...state.channels,
+        [channel]: {
+          isLoading: false,
+          isLoaded: false,
+          isError: true,
+          error: payload.error,
+        },
+      },
+    };
+  }
+
+  return state;
+};
+
+const reducer = handleActions(
+  {
+    [combineActions(
+      fetchGlobalBadgesRequest,
+      fetchGlobalBadgesSuccess,
+      fetchGlobalBadgesFailure,
+    )]: handleFetchFfzGlobalEmotes,
+
+    [combineActions(
+      fetchChannelBadgesRequest,
+      fetchChannelBadgesSuccess,
+      fetchChannelBadgesFailure,
+    )]: handleFetchFfzChannelEmotes,
+  },
+  defaultState,
+);
+
+export default reducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth';
+import badges from './badges';
 import chat from './chat';
 import emotes from './emotes/index';
 import messages from './messages';
 
 export default combineReducers({
   auth,
+  badges,
   chat,
   emotes,
   messages,
