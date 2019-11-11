6d0d422893a9630612e72a5bf698727875576d50
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-11 17:02:09 +0300

    fead: add blocked user selectors

--- src/reducers/chat/selectors.js
+++ src/reducers/chat/selectors.js
@@ -1,4 +1,4 @@
-import { pathOr } from 'ramda';
+import { prop, path, pathOr, map, pipe } from 'ramda';
 
 export const currentChannelSelector = pathOr('', ['chat', 'currentChannel']);
 
@@ -11,3 +11,11 @@ export const channelIdSelector = (state) =>
 
 export const isEvenSelector = (state) =>
   pathOr(false, ['messages', currentChannelSelector(state), 'isEven'], state);
+
+export const isBlockedUsersLoadedSelector = (state) =>
+  state.chat.blockedUsers.isLoaded || state.chat.blockedUsers.isError;
+
+export const blockedUsersSelector = pipe(
+  path(['chat', 'blockedUsers', 'items']),
+  map(prop('name')),
+);
