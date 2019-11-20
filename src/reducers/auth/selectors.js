30033d0dab7492fcab1770216f254afbe2891714
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-20 23:50:04 +0300

    fix: add userDisplayName selector

--- src/reducers/auth/selectors.js
+++ src/reducers/auth/selectors.js
@@ -1,3 +1,4 @@
 export const isAuthSelector = (state) => state.auth.isAuth;
 export const userLoginSelector = (state) => state.auth.user.login;
 export const userIdSelector = (state) => state.auth.user.id;
+export const userDisplayNameSelector = (state) => state.auth.user.displayName;
