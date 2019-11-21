6c05274819c9649f42f688d3ca612dd41b650945
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-22 00:05:31 +0300

    fix: check is auth

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,53 @@
 import React, { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import jwt from 'jsonwebtoken';
 
 import useFetchChatData from 'hooks/useFetchChatData';
 import useCurrentChannel from 'hooks/useCurrentChannel';
 import useTwitchClient from 'hooks/useTwitchClient';
-import { userLoginSelector } from 'reducers/auth/selectors';
+import { isAuthReadySelector } from 'reducers/auth/selectors';
 import { currentChannelSelector } from 'reducers/chat/selectors';
-import { setIsAuth } from 'reducers/auth';
+import { setAuth, fetchUser } from 'reducers/auth';
 import replaceEmojis from 'utils/replaceEmojis';
 import Chat from 'components/Chat';
 
 const Home = () => {
   const dispatch = useDispatch();
 
-  const userLogin = useSelector(userLoginSelector);
+  const isAuthReady = useSelector(isAuthReadySelector);
   const currentChannel = useSelector(currentChannelSelector);
 
-  const client = useTwitchClient(userLogin, localStorage.accessToken);
+  const client = useTwitchClient();
 
   useCurrentChannel();
   useFetchChatData();
 
   useEffect(() => {
-    const rawUser = localStorage.getItem('user');
+    if (isAuthReady) return () => {};
 
-    if (rawUser) {
-      const user = JSON.parse(rawUser);
+    const { user, idToken } = localStorage;
 
-      dispatch(setIsAuth({ isAuth: true, user }));
+    if (!idToken) {
+      dispatch(setAuth({ isAuthReady: true, isAuth: false }));
+      return () => {};
     }
 
-    // TODO: try to connect to the chat. if there is an error, set isAuth to false and connect without login
-  }, [dispatch]);
+    if (user) {
+      const params = {
+        isAuthReady: true,
+        isAuth: true,
+        user: JSON.parse(user),
+      };
+
+      dispatch(setAuth(params));
+    } else {
+      const { sub: id } = jwt.decode(idToken);
+
+      dispatch(fetchUser(id));
+    }
+
+    return () => {};
+  }, [dispatch, isAuthReady]);
 
   const handleSendMessage = useCallback(
     (message) => {
