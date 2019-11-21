8a1ed22089a34d997f50d02959a98ac445d0ca38
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-22 00:15:44 +0300

    Updates

--- precache-manifest.888dc2f657821a853f556bbfaf96e956.js
+++ precache-manifest.888dc2f657821a853f556bbfaf96e956.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a268172c1651c8edf33b6d6c513f4398",
+    "revision": "d88e72463999429e7a816be5583a8513",
     "url": "/chat/index.html"
   },
   {
-    "revision": "64f6eebb66affaace83f",
-    "url": "/chat/static/js/2.0af60c8d.chunk.js"
+    "revision": "b2e77dd16c92abfdeb7d",
+    "url": "/chat/static/js/2.fccab8fc.chunk.js"
   },
   {
-    "revision": "d371eedc3df21cf5a18b",
-    "url": "/chat/static/js/main.43afe3f8.chunk.js"
+    "revision": "de9233e3fdab63122812",
+    "url": "/chat/static/js/main.7d78b917.chunk.js"
   },
   {
     "revision": "966f0adaf27e09321372",
@@ -26,5 +26,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "1cc055edc25e08fa43d1523549f329c0",
     "url": "/chat/static/media/smiley-face.1cc055ed.svg"
+  },
+  {
+    "revision": "75e6e194efc74f52713be6b8f0157f59",
+    "url": "/chat/static/media/twitch.75e6e194.svg"
   }
 ]);
