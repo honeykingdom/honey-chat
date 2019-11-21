615f92d362a8704cc4134426ecc2695088a169dd
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-22 00:01:09 +0300

    fix: use router location for isAuthRedirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
 import React from 'react';
-import pt from 'prop-types';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  RouteComponentProps,
+} from 'react-router-dom';
 
 import isAuthRedirect from 'utils/isAuthRedirect';
 import Home from 'components/Home';
 import Auth from 'components/Auth';
+import Logout from 'components/Logout';
 import AuthCallback from 'components/AuthCallback';
 import GlobalStyles from 'styles';
 
-const getHomeComponent = () =>
-  isAuthRedirect(window.location.hash) ? AuthCallback : Home;
+const getHomeComponent = ({ location }: RouteComponentProps) =>
+  isAuthRedirect(location.hash) ? <AuthCallback /> : <Home />;
 
-const App: React.FC = ({ store }) => (
+interface Props {
+  store: any;
+}
+
+const App: React.FC<Props> = ({ store }) => (
   <>
     <Provider store={store}>
       <Router>
         <Switch>
-          <Route exact path="/chat/" component={getHomeComponent()} />
+          <Route exact path="/chat/" render={getHomeComponent} />
           <Route exact path="/chat/auth" component={Auth} />
+          <Route exact path="/chat/logout" component={Logout} />
         </Switch>
       </Router>
     </Provider>
     <GlobalStyles />
   </>
 );
-
-App.propTypes = {
-  store: pt.shape({}).isRequired,
-};
 
 export default App;
