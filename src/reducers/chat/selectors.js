f5c04e86baf06d44db307b7d57031a9d231f2e19
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-22 00:02:18 +0300

    feat: add some user selectors

--- src/reducers/chat/selectors.js
+++ src/reducers/chat/selectors.js
@@ -1,4 +1,11 @@
 import * as R from 'ramda';
+import { createSelector } from 'reselect';
+
+import {
+  globalBadgesSelector,
+  channelBadgesSelector,
+} from 'reducers/badges/selectors';
+import createBadges from 'utils/createBadges';
 
 export const currentChannelSelector = R.pathOr('', ['chat', 'currentChannel']);
 
@@ -15,3 +22,25 @@ export const isBlockedUsersLoadedSelector = (state) =>
   state.chat.blockedUsers.isLoaded || state.chat.blockedUsers.isError;
 
 export const blockedUsersSelector = (state) => state.chat.blockedUsers.items;
+
+export const userColorSelector = (state) =>
+  R.pathOr(
+    '',
+    ['chat', 'channels', currentChannelSelector(state), 'userState', 'color'],
+    state,
+  );
+
+export const userBadgesSelector = (state) =>
+  R.pathOr(
+    {},
+    ['chat', 'channels', currentChannelSelector(state), 'userState', 'badges'],
+    state,
+  );
+
+export const userBadgesImagesSelector = createSelector(
+  userBadgesSelector,
+  globalBadgesSelector,
+  channelBadgesSelector,
+  (userBadges, globalBadges, channelBadges) =>
+    createBadges(userBadges, globalBadges, channelBadges),
+);
