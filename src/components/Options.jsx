b2a656030f3de4a2310f4ad5913ac02ced87406d
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-22 00:07:25 +0300

    feat: add profile and logout button

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
+import pt from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-// import pt from 'prop-types';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { changeOption } from 'reducers/options';
@@ -41,8 +42,38 @@ const Option = styled.div`
   cursor: pointer;
 `;
 const OptionText = styled.span``;
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const UserName = styled.div`
+  font-weight: bold;
+  color: ${(p) => p.color};
+`;
+const LogOutButton = styled.a`
+  margin-left: auto;
+  color: #bf94ff;
+  text-decoration: none;
+  cursor: pointer;
 
-const Options = () => {
+  &:focus,
+  &:hover {
+    color: #a970ff;
+    text-decoration: underline;
+  }
+
+  &:visited {
+    color: #a970ff;
+  }
+`;
+const Badge = styled.img`
+  margin-right: 3px;
+  max-width: 100%;
+  vertical-align: middle;
+  border-radius: 3px;
+`;
+
+const Options = ({ isAuth, userDisplayName, userColor, userBadgesImages }) => {
   const dispatch = useDispatch();
   const options = useSelector(optionsSelector);
 
@@ -65,6 +96,30 @@ const Options = () => {
     [dispatch],
   );
 
+  const renderProfileCategory = () => (
+    <Category>
+      <CategoryHeader>Profile</CategoryHeader>
+      <CategoryItems>
+        <Profile>
+          {userBadgesImages.map(({ alt, label, src, srcSet }, key) => (
+            <Badge
+              // eslint-disable-next-line react/no-array-index-key
+              key={key}
+              alt={alt}
+              aria-label={label}
+              src={src}
+              srcSet={srcSet}
+            />
+          ))}
+          <UserName color={userColor}>{userDisplayName}</UserName>
+          <LogOutButton as={Link} to="/chat/logout">
+            Log Out
+          </LogOutButton>
+        </Profile>
+      </CategoryItems>
+    </Category>
+  );
+
   const renderOptionsCategory = useCallback(
     ({ title, items }, key) => (
       <Category key={key}>
@@ -78,13 +133,25 @@ const Options = () => {
   return (
     <OptionsRoot>
       <Header>Chat settings</Header>
-      <Categories>{options.map(renderOptionsCategory)}</Categories>
+      <Categories>
+        {isAuth && renderProfileCategory()}
+        {options.map(renderOptionsCategory)}
+      </Categories>
     </OptionsRoot>
   );
 };
 
-Options.defaultProps = {};
+Options.defaultProps = {
+  userDisplayName: '',
+  userColor: '',
+  userBadgesImages: [],
+};
 
-Options.propTypes = {};
+Options.propTypes = {
+  isAuth: pt.bool.isRequired,
+  userDisplayName: pt.string,
+  userColor: pt.string,
+  userBadgesImages: pt.arrayOf(pt.shape({})),
+};
 
 export default Options;
