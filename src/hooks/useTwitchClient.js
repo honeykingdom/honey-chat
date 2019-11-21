b7678371b07fc9bf995c67b9d769732b8d28a0b8
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-22 00:09:33 +0300

    fix: add client disconnect calls

--- src/hooks/useTwitchClient.js
+++ src/hooks/useTwitchClient.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useMemo } from 'react';
+import { useEffect, useRef, useMemo, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import uuid from 'uuid/v4';
 import { Client } from 'twitch-simple-irc';
@@ -16,75 +16,124 @@ import {
   updateUserState,
   updateRoomState,
 } from 'reducers/chat';
-import { currentChannelSelector } from 'reducers/chat/selectors';
-import { isAuthSelector, userIdSelector } from 'reducers/auth/selectors';
+import {
+  currentChannelSelector,
+  isConnectedSelector,
+} from 'reducers/chat/selectors';
+import {
+  isAuthSelector,
+  userIdSelector,
+  isAuthReadySelector,
+  userLoginSelector,
+} from 'reducers/auth/selectors';
+import { setAuth } from 'reducers/auth';
 import { NOTICE_MESSAGE_TAGS } from 'utils/constants';
 import createOwnMessage from 'utils/createOwnMessage';
 
-const useTwitchClient = (name, auth) => {
+const emptyFunc = () => {};
+
+const useTwitchClient = () => {
   const dispatch = useDispatch();
 
+  const isAuthReady = useSelector(isAuthReadySelector);
+  const isAuth = useSelector(isAuthSelector);
+  const userLogin = useSelector(userLoginSelector);
+  const userId = useSelector(userIdSelector);
+  const isConnected = useSelector(isConnectedSelector);
   const currentChannel = useSelector(currentChannelSelector);
   const prevChannel = usePrevious(currentChannel);
-  const userId = useSelector(userIdSelector);
-  const isAuth = useSelector(isAuthSelector);
   const clientRef = useRef(null);
 
-  useEffect(() => {
-    if (!currentChannel) return () => {};
+  const registerEvents = useCallback(
+    (client) => {
+      const handleRegister = () => dispatch(setIsConnected(true));
+      const handleDisconnect = () => dispatch(setIsConnected(false));
+      const handleGlobalUserState = (data) =>
+        dispatch(updateGlobalUserState(data));
+      const handleUserState = (data) => dispatch(updateUserState(data));
+      const handleRoomState = (data) => dispatch(updateRoomState(data));
+      const handleMessage = (data) => dispatch(addMessage(data));
+      const handleNotice = (data) => {
+        if (data.message === 'Login authentication failed') {
+          dispatch(setAuth({ isAuth: false }));
+          client.current.disconnect();
+          // eslint-disable-next-line no-param-reassign
+          client.current = null;
+          return;
+        }
 
-    const handleRegister = () => dispatch(setIsConnected(true));
-    const handleDisconnect = () => dispatch(setIsConnected(true));
-    const handleGlobalUserState = (data) =>
-      dispatch(updateGlobalUserState(data));
-    const handleUserState = (data) => dispatch(updateUserState(data));
-    const handleRoomState = (data) => dispatch(updateRoomState(data));
-    const handleMessage = (data) => dispatch(addMessage(data));
-    const handleNotice = (data) => {
-      const normalizedMessage = { ...data, tags: { ...data.tags, id: uuid() } };
-      dispatch(addNoticeMessage(normalizedMessage));
+        const normalizedMessage = {
+          ...data,
+          tags: { ...data.tags, id: uuid() },
+        };
+
+        dispatch(addNoticeMessage(normalizedMessage));
+      };
+      const handleUserNotice = (data) => dispatch(addUserNoticeMessage(data));
+      const handleClearChat = (data) => {
+        if (!data.tags.targetUserId) return;
+        dispatch(clearChat(data));
+      };
+
+      client.current.on('register', handleRegister);
+      client.current.on('disconnect', handleDisconnect);
+      client.current.on('globaluserstate', handleGlobalUserState);
+      client.current.on('userstate', handleUserState);
+      client.current.on('roomstate', handleRoomState);
+      client.current.on('message', handleMessage);
+      client.current.on('notice', handleNotice);
+      client.current.on('usernotice', handleUserNotice);
+      client.current.on('clearchat', handleClearChat);
+    },
+    [dispatch],
+  );
+
+  useEffect(() => {
+    return () => {
+      if (clientRef.current) {
+        clientRef.current.disconnect();
+        clientRef.current = null;
+      }
     };
-    const handleUserNotice = (data) => dispatch(addUserNoticeMessage(data));
-    const handleClearChat = (data) => {
-      if (!data.tags.targetUserId) return;
-      dispatch(clearChat(data));
-    };
+  }, [clientRef]);
+
+  useEffect(() => {
+    if (!currentChannel || !isAuthReady) return emptyFunc;
 
     if (!clientRef.current) {
+      const options = isAuth
+        ? { name: userLogin, auth: localStorage.accessToken }
+        : {};
+
       (async () => {
-        clientRef.current = new Client(isAuth ? { name, auth } : {});
+        clientRef.current = new Client(options);
+
+        registerEvents(clientRef);
 
         await clientRef.current.connect();
 
         clientRef.current.join(currentChannel);
       })();
-    } else if (prevChannel !== currentChannel) {
+
+      return emptyFunc;
+    }
+
+    if (prevChannel !== currentChannel) {
       clientRef.current.part(prevChannel);
       clientRef.current.join(currentChannel);
     }
 
-    clientRef.current.on('register', handleRegister);
-    clientRef.current.on('disconnect', handleDisconnect);
-    clientRef.current.on('globaluserstate', handleGlobalUserState);
-    clientRef.current.on('userstate', handleUserState);
-    clientRef.current.on('roomstate', handleRoomState);
-    clientRef.current.on('message', handleMessage);
-    clientRef.current.on('notice', handleNotice);
-    clientRef.current.on('usernotice', handleUserNotice);
-    clientRef.current.on('clearchat', handleClearChat);
-
-    return () => {
-      clientRef.current.off('register', handleRegister);
-      clientRef.current.off('disconnect', handleDisconnect);
-      clientRef.current.off('globaluserstate', handleGlobalUserState);
-      clientRef.current.off('userstate', handleUserState);
-      clientRef.current.off('roomstate', handleRoomState);
-      clientRef.current.off('message', handleMessage);
-      clientRef.current.off('notice', handleNotice);
-      clientRef.current.off('usernotice', handleUserNotice);
-      clientRef.current.off('clearchat', handleClearChat);
-    };
-  }, [dispatch, name, auth, currentChannel, prevChannel, isAuth]);
+    return emptyFunc;
+  }, [
+    dispatch,
+    registerEvents,
+    isAuth,
+    isAuthReady,
+    isConnected,
+    userLogin,
+    currentChannel,
+    prevChannel,
+  ]);
 
   const client = () => ({
     say(channel, message) {
@@ -94,7 +143,13 @@ const useTwitchClient = (name, auth) => {
 
       function handleUserState(data) {
         if (data.channel === channel) {
-          const m = createOwnMessage(message, data.tags, channel, name, userId);
+          const m = createOwnMessage(
+            message,
+            data.tags,
+            channel,
+            userLogin,
+            userId,
+          );
           dispatch(addMessage(m));
           // eslint-disable-next-line no-use-before-define
           removeListeners();
@@ -123,7 +178,7 @@ const useTwitchClient = (name, auth) => {
     },
   });
 
-  return useMemo(client, [clientRef, dispatch, name, userId]);
+  return useMemo(client, [clientRef, dispatch, userLogin, userId]);
 };
 
 export default useTwitchClient;
