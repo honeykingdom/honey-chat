63bc53271041e7dec083a3b64fa9d165eeb46fb8
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-26 02:06:02 +0300

    feat: add emotes search

--- src/features/chat/components/EmotePicker.tsx
+++ src/features/chat/components/EmotePicker.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
+import { RootState } from 'app/rootReducer';
 import Scrollbar from 'components/Scrollbar';
 import { emoteCategoriesSelector } from 'features/chat/selectors';
 import { EmoteCategory } from 'features/chat/utils/createEmoteCategories';
+import { ReactComponent as SearchIconSvg } from 'icons/search.svg';
 
 const EmotePickerRoot = styled.div`
-  padding-top: 32px;
+  padding-top: 30px;
   padding-bottom: 16px;
   height: 100%;
 `;
 const Categories = styled.div`
-  height: 100%;
+  height: calc(100% - 30px);
 `;
 const Category = styled.div`
   padding-top: 10px;
@@ -42,13 +44,61 @@ const Emote = styled.img`
     cursor: pointer;
   }
 `;
+const SearchBlock = styled.div`
+  position: relative;
+  padding: 0 16px;
+  color: #adadb8;
+`;
+const SearchInput = styled.input`
+  padding-left: 30px;
+  padding-right: 10px;
+  width: 100%;
+  height: 30px;
+  color: #efeff1;
+  font-family: inherit;
+  line-height: 1.5;
+  background-clip: padding-box;
+  background-color: rgba(255, 255, 255, 0.15);
+  border: 2px solid rgba(255, 255, 255, 0.15);
+  outline: 0;
+  border-radius: 4px;
+  appearance: none;
+  transition: box-shadow 0.1s ease-in, border 0.1s ease-in,
+    background-color 0.1s ease-in;
+
+  &:focus {
+    background-color: #000;
+    border-color: #9147ff;
+  }
+`;
+const SearchIcon = styled(SearchIconSvg)`
+  position: absolute;
+  top: 50%;
+  left: 21px;
+  display: block;
+  width: 20px;
+  height: 20px;
+  transform: translateY(-50%);
+`;
 
 interface Props {
   onEmoteClick: (name: string) => void;
 }
 
 const EmotePicker = ({ onEmoteClick }: Props) => {
-  const emoteCategories = useSelector(emoteCategoriesSelector);
+  const [search, setSearch] = useState('');
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const emoteCategories = useSelector((state: RootState) =>
+    emoteCategoriesSelector(state, search),
+  );
+
+  useEffect(() => {
+    if (searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+
+    return () => setSearch('');
+  }, []);
 
   const renderCategory = ({ title, items }: EmoteCategory, key: number) => (
     <Category key={key}>
@@ -69,6 +119,15 @@ const EmotePicker = ({ onEmoteClick }: Props) => {
 
   return (
     <EmotePickerRoot>
+      <SearchBlock>
+        <SearchIcon />
+        <SearchInput
+          ref={searchInputRef}
+          placeholder="Search for Emotes"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </SearchBlock>
       <Categories>
         <Scrollbar>{emoteCategories.map(renderCategory)}</Scrollbar>
       </Categories>
