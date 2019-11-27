a31d8ad8883edbaf9cda36c83d1ce2cec465ac16
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-28 00:15:05 +0300

    feat: add emotes usage statistic functions

---
+++ src/features/chat/utils/emotesUsageStatistic.test.js
@@ -0,0 +1,189 @@
+import * as R from 'ramda';
+
+import { emotes, findTwitchEmote, findBttvEmote, findFfzEmote } from 'mocks';
+import { LS_EMOTES_USAGE_STATISTIC } from 'utils/constants';
+import parseMessageEntities from 'features/chat/utils/parseMessageEntities';
+import {
+  readEmotesUsageStatistic,
+  writeEmotesUsageStatistic,
+  getUsageStatisticFromEntities,
+  getEmotesFromUsageStatistic,
+} from 'features/chat/utils/emotesUsageStatistic';
+
+const mapFindEmote = {
+  'twitch-emote': findTwitchEmote,
+  'bttv-emote': findBttvEmote,
+  'ffz-emote': findFfzEmote,
+};
+
+const createUsage = ([type, name, uses, lastUpdatedAt]) => {
+  const emote = mapFindEmote[type](name);
+
+  return {
+    [emote.id]: {
+      type,
+      id: emote.id,
+      lastUpdatedAt: lastUpdatedAt || Date.now(),
+      uses,
+    },
+  };
+};
+
+const createStatistic = R.pipe(
+  R.groupBy(R.head),
+  R.map(R.pipe(R.map(createUsage), R.mergeAll)),
+);
+
+describe('emotes usage statistic', () => {
+  jest.spyOn(global.Date, 'now').mockImplementation(() => 1500000000000);
+
+  beforeEach(() => {
+    localStorage.clear();
+    localStorage.setItem.mockClear();
+  });
+
+  it('should read empty statistic', () => {
+    expect(readEmotesUsageStatistic()).toEqual({});
+  });
+
+  it('should read statistic', () => {
+    const result1 = createStatistic([
+      ['twitch-emote', 'Kappa', 4],
+      ['bttv-emote', 'sumSmash', 2],
+      ['ffz-emote', 'KKonaW', 1],
+    ]);
+
+    localStorage.setItem(LS_EMOTES_USAGE_STATISTIC, JSON.stringify(result1));
+
+    expect(readEmotesUsageStatistic()).toEqual(result1);
+  });
+
+  it('should not write empty statistic', () => {
+    writeEmotesUsageStatistic(['hello world']);
+
+    expect(localStorage.setItem).not.toHaveBeenCalled();
+  });
+
+  it('should write statistic', () => {
+    const message = '4Head EZ 4Head KKonaW KKonaW test message KKonaW KKonaW';
+    const entities = parseMessageEntities(message, emotes, null, true);
+
+    const result = createStatistic([
+      ['twitch-emote', '4Head', 2],
+      ['bttv-emote', 'EZ', 1],
+      ['ffz-emote', 'KKonaW', 4],
+    ]);
+
+    writeEmotesUsageStatistic(entities);
+
+    expect(JSON.parse(localStorage.getItem(LS_EMOTES_USAGE_STATISTIC))).toEqual(
+      result,
+    );
+  });
+
+  it('should merge statistic', () => {
+    const initialData = createStatistic([
+      ['twitch-emote', '4Head', 2],
+      ['bttv-emote', 'EZ', 1],
+      ['ffz-emote', 'KKonaW', 4],
+    ]);
+
+    localStorage.setItem(
+      LS_EMOTES_USAGE_STATISTIC,
+      JSON.stringify(initialData),
+    );
+
+    const message = '4Head KKona EZ KKonaW @twitch';
+    const entities = parseMessageEntities(message, emotes, null, true);
+
+    const result = createStatistic([
+      ['twitch-emote', '4Head', 3],
+      ['bttv-emote', 'KKona', 1],
+      ['bttv-emote', 'EZ', 2],
+      ['ffz-emote', 'KKonaW', 5],
+    ]);
+
+    writeEmotesUsageStatistic(entities);
+    expect(JSON.parse(localStorage.getItem(LS_EMOTES_USAGE_STATISTIC))).toEqual(
+      result,
+    );
+  });
+
+  it('should return statistic from entities', () => {
+    const message =
+      'Kappa Keepo Kappa 4Head hello @twitch :) KKona KKonaW Zappa EZ sumSmash';
+    const entities = parseMessageEntities(message, emotes, null, true);
+
+    const result = createStatistic([
+      ['twitch-emote', 'Kappa', 2],
+      ['twitch-emote', 'Keepo', 1],
+      ['twitch-emote', '4Head', 1],
+      ['twitch-emote', ':)', 1],
+      ['bttv-emote', 'KKona', 1],
+      ['bttv-emote', 'EZ', 1],
+      ['bttv-emote', 'Zappa', 1],
+      ['bttv-emote', 'sumSmash', 1],
+      ['ffz-emote', 'KKonaW', 1],
+    ]);
+
+    localStorage.setItem(LS_EMOTES_USAGE_STATISTIC, JSON.stringify(result));
+
+    expect(getUsageStatisticFromEntities(entities)).toEqual(result);
+  });
+
+  it('should return empty emotes list from statistic', () => {
+    expect(getEmotesFromUsageStatistic(emotes)).toEqual([]);
+  });
+
+  it('should return emotes list from statistic', () => {
+    // prettier-ignore
+    const storageStatistic = createStatistic([
+      ['twitch-emote', 'Keepo',    1, 1500000000011],
+      ['twitch-emote', '4Head',    2, 1500000000001],
+      ['twitch-emote', 'Kappa',    5, 1500000000000],
+      ['bttv-emote',   'KKona',    1, 1500000000010],
+      ['bttv-emote',   'sumSmash', 3, 1500000000001],
+      ['ffz-emote',    'KKonaW',   2, 1500000000002],
+    ]);
+
+    localStorage.setItem(
+      LS_EMOTES_USAGE_STATISTIC,
+      JSON.stringify(storageStatistic),
+    );
+
+    const result = [
+      findTwitchEmote('Kappa'),
+      findBttvEmote('sumSmash'),
+      findFfzEmote('KKonaW'),
+      findTwitchEmote('4Head'),
+      findTwitchEmote('Keepo'),
+      findBttvEmote('KKona'),
+    ];
+
+    expect(getEmotesFromUsageStatistic(emotes)).toEqual(result);
+  });
+
+  it('should return emotes list from statistic with limit', () => {
+    const storageStatistic = createStatistic([
+      ['twitch-emote', 'Keepo', 1],
+      ['twitch-emote', '4Head', 2],
+      ['twitch-emote', 'Kappa', 5],
+      ['bttv-emote', 'KKona', 1],
+      ['bttv-emote', 'sumSmash', 3],
+      ['ffz-emote', 'KKonaW', 1],
+    ]);
+
+    localStorage.setItem(
+      LS_EMOTES_USAGE_STATISTIC,
+      JSON.stringify(storageStatistic),
+    );
+
+    const result = [
+      findTwitchEmote('Kappa'),
+      findBttvEmote('sumSmash'),
+      findTwitchEmote('4Head'),
+    ];
+
+    expect(getEmotesFromUsageStatistic(emotes, 3)).toEqual(result);
+  });
+});
---
+++ src/features/chat/utils/emotesUsageStatistic.ts
@@ -0,0 +1,120 @@
+import * as R from 'ramda';
+
+import { LS_EMOTES_USAGE_STATISTIC } from 'utils/constants';
+import * as htmlEntity from 'features/chat/utils/htmlEntity';
+import findEmote from 'features/chat/utils/findEmote';
+import { StateEmotes } from 'features/chat/selectors';
+import { MessageEntity } from 'features/chat/slice/messages';
+
+type Type = 'twitch-emote' | 'bttv-emote' | 'ffz-emote';
+
+type AUsageItem = {
+  lastUpdatedAt: number;
+  uses: number;
+};
+
+type UsageItemTwitchFfz = {
+  type: 'twitch-emote' | 'ffz-emote';
+  id: number;
+} & AUsageItem;
+
+type UsageItemBttv = {
+  type: 'bttv-emote';
+  id: string;
+} & AUsageItem;
+
+type UsageItem = UsageItemTwitchFfz | UsageItemBttv;
+
+type EmotesUsageStatistic = Record<Type, Record<string, UsageItem>>;
+
+export const readEmotesUsageStatistic = (): Partial<EmotesUsageStatistic> => {
+  try {
+    const statistic = localStorage.getItem(LS_EMOTES_USAGE_STATISTIC);
+
+    return JSON.parse(statistic as string) || {};
+  } catch (e) {
+    return {};
+  }
+};
+
+export const getUsageStatisticFromEntities = R.pipe<
+  MessageEntity[],
+  any,
+  any,
+  any
+>(
+  R.filter((v: any) =>
+    R.includes(R.prop('type', v), ['twitch-emote', 'bttv-emote', 'ffz-emote']),
+  ),
+  R.groupBy<any>(R.prop('type')),
+  R.map(
+    R.pipe<any, any, any>(
+      R.groupBy<any>(R.prop('id')),
+      R.map((items: any) => ({
+        type: items[0].type,
+        id: items[0].id,
+        lastUpdatedAt: Date.now(),
+        uses: items.length,
+      })),
+    ),
+  ),
+);
+
+const mergeFn = (key: string, left: any, right: any) =>
+  key === 'uses' ? left + right : right;
+
+export const writeEmotesUsageStatistic = (entries: MessageEntity[]) => {
+  const statistic = getUsageStatisticFromEntities(entries) as Partial<
+    EmotesUsageStatistic
+  >;
+
+  if (R.isEmpty(statistic)) return;
+
+  const history = readEmotesUsageStatistic();
+
+  const newHistory = R.mergeDeepWithKey(mergeFn, history, statistic);
+
+  localStorage.setItem(LS_EMOTES_USAGE_STATISTIC, JSON.stringify(newHistory));
+};
+
+const normalizeHistory = R.pipe<any, any, any, any, UsageItem[]>(
+  R.values,
+  R.map(R.values),
+  R.flatten,
+  R.sortWith([R.descend(R.prop('uses')), R.descend(R.prop('lastUpdatedAt'))]),
+);
+
+export const getEmotesFromUsageStatistic = (
+  emotes: StateEmotes,
+  limit = 27,
+) => {
+  if (!emotes) return [];
+
+  const result: htmlEntity.Emote[] = [];
+  const history = R.pipe(readEmotesUsageStatistic, normalizeHistory)();
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const item of history) {
+    if (result.length === limit) return result;
+
+    let emoteEntity = null;
+
+    if (item.type === 'twitch-emote') {
+      emoteEntity = findEmote.twitch.byId(item.id, emotes);
+    }
+
+    if (item.type === 'bttv-emote') {
+      emoteEntity = findEmote.bttv.byId(item.id, emotes);
+    }
+
+    if (item.type === 'ffz-emote') {
+      emoteEntity = findEmote.ffz.byId(item.id, emotes);
+    }
+
+    if (emoteEntity) {
+      result.push(emoteEntity);
+    }
+  }
+
+  return result;
+};
