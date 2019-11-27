adae15816b90275c16c041511b321cfcfcfb89a0
DmitryScaletta <dmitryscaletta@mail.ru>
2019-11-28 00:17:13 +0300

    feat: add test helpers

---
+++ src/mocks/index.ts
@@ -0,0 +1,38 @@
+import * as api from 'api';
+import twitchEmotes from 'mocks/twitchEmotes';
+import bttvGlobal from 'mocks/bttvGlobalEmotes';
+import bttvChannel from 'mocks/bttvChannelEmotes';
+import ffzGlobal from 'mocks/ffzGlobalEmotes';
+import ffzChannel from 'mocks/ffzChannelEmotes';
+import {
+  parseTwitchGlobalEmotes,
+  parseTwitchChannelEmotes,
+  parseBttvGlobalEmotes,
+  parseBttvChannelEmotes,
+  parseFfzGlobalEmotes,
+  parseFfzChannelEmotes,
+} from 'features/chat/utils/parseApiResponse';
+import findEmote from 'features/chat/utils/findEmote';
+
+export const emotes = {
+  twitchGlobal: parseTwitchGlobalEmotes(twitchEmotes),
+  twitchUser: parseTwitchChannelEmotes(twitchEmotes),
+  bttvGlobal: parseBttvGlobalEmotes(bttvGlobal as api.BttvGlobalEmotesResponse),
+  bttvChannel: parseBttvChannelEmotes(
+    bttvChannel as api.BttvChannelEmotesResponse,
+  ),
+  ffzGlobal: parseFfzGlobalEmotes(ffzGlobal),
+  ffzChannel: parseFfzChannelEmotes(ffzChannel),
+};
+
+export const findTwitchEmote = (name: string) =>
+  findEmote.twitch.byName(name, emotes);
+
+export const findBttvEmote = (name: string) =>
+  findEmote.bttv.byName(name, emotes);
+
+export const findFfzEmote = (name: string) =>
+  findEmote.ffz.byName(name, emotes);
+
+export const getEmojiUrl = (code: string) =>
+  `https://twemoji.maxcdn.com/v/latest/72x72/${code}.png`;
