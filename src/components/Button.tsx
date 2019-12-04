24bac6b0bd6e78be4779ca1d547593cc2e8e0d5c
DmitryScaletta <dmitryscaletta@mail.ru>
2019-12-05 00:04:34 +0300

    feat: add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const Button = styled.button.attrs({ type: 'button' })`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 10px;
+  height: 30px;
+  border: none;
+  color: #fff;
+  background-color: #9147ff;
+  outline: none;
+  font-size: 12px;
+  border-radius: 4px;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #772ce8;
+  }
+
+  &:active {
+    background-color: #5c16c5;
+  }
+
+  &:focus {
+    box-shadow: 0 0 6px 0 #772ce8;
+  }
+
+  &[disabled] {
+    background-color: rgba(255, 255, 255, 0.26);
+    color: rgba(255, 255, 255, 0.8);
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+export default Button;
