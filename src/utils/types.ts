9be093a85ca2e622facf0c2aba962b9a89c3a466
DmitryScaletta <dmitryscaletta@mail.ru>
2020-05-06 12:19:39 +0300

    refact: add FetchStatus type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,9 @@
 import { SerializedError } from '@reduxjs/toolkit';
 
+export type FetchStatus = 'idle' | 'loading' | 'success' | 'error';
+
 export type FetchResult<T> = {
-  status: 'idle' | 'loading' | 'success' | 'error';
+  status: FetchStatus;
   error: SerializedError;
   items: T;
 };
