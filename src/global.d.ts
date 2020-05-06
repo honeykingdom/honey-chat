2a543b5bc471c0f70b47419f37c2e38f55c3eb21
DmitryScaletta <dmitryscaletta@mail.ru>
2020-05-06 13:28:15 +0300

    feat: add global type definitions

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,24 @@
+declare module '*.ogg' {
+  const url: string;
+  export default url;
+}
+
+declare module 'emojilib' {
+  interface Emoji {
+    keywords: string[];
+    char: string;
+    fitzpatrick_scale: boolean;
+    category: string;
+  }
+
+  export const lib: Record<string, Emoji>;
+
+  export const ordered: string[];
+
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  export const fitzpatrick_scale_modifiers: string[];
+}
+
 declare module 'twemoji-parser' {
   export type EmojiEntity = {
     type: string;
