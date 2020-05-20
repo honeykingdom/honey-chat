4d79c6ac9b80f2e79974558315399a8c17c4077f
DmitryScaletta <dmitryscaletta@mail.ru>
2020-05-21 00:52:05 +0300

    feat: add blocked users feature

---
+++ src/features/blockedUsers/blockedUsersSlice.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+import * as api from 'api';
+import type { FetchResult } from 'utils/types';
+import { parseBlockedUsers } from 'features/chat/utils/parseApiResponse';
+
+type BlockedUsersState = FetchResult<string[]>;
+
+const initialState: BlockedUsersState = {
+  status: 'idle',
+  error: {},
+  items: [],
+};
+
+export const fetchBlockedUsers = createAsyncThunk(
+  'chat/fetchBlockedUsers',
+  (userId: string) => api.fetchBlockedUsers(userId),
+);
+
+const blockedUsers = createSlice({
+  name: 'blockedUsers',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchBlockedUsers.pending, (state) => {
+      state.status = 'loading';
+      state.error = {};
+    });
+
+    builder.addCase(fetchBlockedUsers.fulfilled, (state, { payload }): void => {
+      state.status = 'success';
+      state.items = parseBlockedUsers(payload);
+    });
+
+    builder.addCase(fetchBlockedUsers.rejected, (state, { error }): void => {
+      state.status = 'error';
+      state.error = error;
+    });
+  },
+});
+
+export default blockedUsers.reducer;
