d0a3e0ad8d33fd1e4fdadc999f21a9f56cd8893d
DmitryScaletta <dmitryscaletta@mail.ru>
2020-05-21 01:04:26 +0300

    feat: add all reducers

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,12 +1,20 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import auth from 'features/auth/authSlice';
-import chat from 'features/chat/slice/chatSlice';
+import chat from 'features/chat/chatSlice';
+import messages from 'features/messages/messagesSlice';
+import emotes from 'features/emotes/emotesSlice';
+import badges from 'features/badges/badgesSlice';
+import blockedUsers from 'features/blockedUsers/blockedUsersSlice';
 import options from 'features/options/optionsSlice';
 
 const rootReducer = combineReducers({
   auth,
   chat,
+  messages,
+  emotes,
+  badges,
+  blockedUsers,
   options,
 });
 
