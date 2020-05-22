18e5a68835b97c01d8116053cced270b730459f1
DmitryScaletta <dmitryscaletta@mail.ru>
2020-05-22 13:25:10 +0300

    fix: add message cards to the root reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -3,6 +3,7 @@ import { combineReducers } from '@reduxjs/toolkit';
 import auth from 'features/auth/authSlice';
 import chat from 'features/chat/chatSlice';
 import messages from 'features/messages/messagesSlice';
+import messageCards from 'features/messageCards/messageCardsSlice';
 import emotes from 'features/emotes/emotesSlice';
 import badges from 'features/badges/badgesSlice';
 import blockedUsers from 'features/blockedUsers/blockedUsersSlice';
@@ -12,6 +13,7 @@ const rootReducer = combineReducers({
   auth,
   chat,
   messages,
+  messageCards,
   emotes,
   badges,
   blockedUsers,
