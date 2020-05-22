2891c8b688e3687d6005f64ab49cdfdfbe37cc86
DmitryScaletta <dmitryscaletta@mail.ru>
2020-05-22 14:24:29 +0300

    fix: remove twemoji-parser types

--- src/global.d.ts
+++ src/global.d.ts
@@ -18,26 +18,3 @@ declare module 'emojilib' {
   // eslint-disable-next-line @typescript-eslint/camelcase
   export const fitzpatrick_scale_modifiers: string[];
 }
-
-declare module 'twemoji-parser' {
-  export type EmojiEntity = {
-    type: string;
-    text: string;
-    url: string;
-    indices: number[];
-  };
-
-  export type ParsingOptions = {
-    buildUrl?: (codepoints: string, assetType: string) => string;
-    assetType?: 'png' | 'svg';
-  };
-
-  export const TypeName = 'emoji';
-
-  export function parse(
-    text: string,
-    options?: ParsingOptions,
-  ): Array<EmojiEntity>;
-
-  export function toCodePoints(unicodeSurrogates: string): Array<string>;
-}
