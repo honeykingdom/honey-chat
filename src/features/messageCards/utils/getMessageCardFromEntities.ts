456a4340904dc40793cfcf6b97c6d321e05353ba
DmitryScaletta <dmitryscaletta@mail.ru>
2020-05-22 13:25:37 +0300

    feat: add getMessageCardFromEntities function

---
+++ src/features/messageCards/utils/getMessageCardFromEntities.ts
@@ -0,0 +1,61 @@
+import type {
+  MessageEntity,
+  MessageCardInfo,
+} from 'features/messages/messagesSlice';
+
+// TODO: add mobile twitch links
+
+// https://regex101.com/r/jGbDV1/2
+const twitchClipRegex = /^(?:https?:)?(?:\/\/)?(?:clips\.twitch\.tv\/|(?:www\.)?twitch\.tv\/(?:[\d\w]+)\/clip\/)([\d\w]+)/;
+
+// https://regex101.com/r/xsgeA4/2
+const twitchVideoRegex = /^(?:https?:)?(?:\/\/)?(?:www\.)?twitch.tv\/videos\/(\d+)/;
+
+// https://regexr.com/3dj5t
+const youtubeVideoRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?(?:youtube\.com|youtu.be)(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/;
+
+const getMessageCardFromEntities = (
+  entities: MessageEntity[],
+): MessageCardInfo | null => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const entity of entities) {
+    if (typeof entity === 'object' && entity.type === 'link') {
+      // twitch clip
+      let m = twitchClipRegex.exec(entity.text);
+
+      if (m) {
+        return {
+          type: 'twitch-clip',
+          id: m[1],
+          url: entity.href,
+        };
+      }
+
+      // twitch video
+      m = twitchVideoRegex.exec(entity.text);
+
+      if (m) {
+        return {
+          type: 'twitch-video',
+          id: m[1],
+          url: entity.href,
+        };
+      }
+
+      // youtube video
+      m = youtubeVideoRegex.exec(entity.text);
+
+      if (m) {
+        return {
+          type: 'youtube-video',
+          id: m[5],
+          url: entity.href,
+        };
+      }
+    }
+  }
+
+  return null;
+};
+
+export default getMessageCardFromEntities;
