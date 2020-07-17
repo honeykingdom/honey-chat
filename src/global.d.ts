de49f1e4c25166d89b1f436bd26a01bcd7eced91
DmitryScaletta <dmitryscaletta@mail.ru>
2020-07-17 12:46:37 +0300

    feat: add typings

--- src/global.d.ts
+++ src/global.d.ts
@@ -15,19 +15,3 @@ declare module 'features/emotes/emojisMap.json' {
 
   export default data;
 }
-
-declare module 'emojilib' {
-  interface Emoji {
-    keywords: string[];
-    char: string;
-    fitzpatrick_scale: boolean;
-    category: string;
-  }
-
-  export const lib: Record<string, Emoji>;
-
-  export const ordered: string[];
-
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  export const fitzpatrick_scale_modifiers: string[];
-}
