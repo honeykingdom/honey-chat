28a8a481519ae9f738a96eedc3d263dece893128
DmitryScaletta <dmitryscaletta@mail.ru>
2020-08-23 01:16:45 +0300

    feat: add new recent messages service

--- src/api/recentMessages.ts
+++ src/api/recentMessages.ts
@@ -8,7 +8,14 @@ export interface RecentMessagesResponse {
 // eslint-disable-next-line import/prefer-default-export
 export const fetchRecentMessages = (
   channel: string,
-): Promise<RecentMessagesResponse> =>
-  fetchRequest(
+): Promise<RecentMessagesResponse> => {
+  if (channel === 'nasvay_s') {
+    return fetchRequest(
+      `https://honeykingdom.herokuapp.com/api/v1/recent-messages/${channel}`,
+    );
+  }
+
+  return fetchRequest(
     `https://recent-messages.robotty.de/api/v2/recent-messages/${channel}?clearchatToNotice=true`,
   );
+};
