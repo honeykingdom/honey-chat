c8603c24c662bee7bf0c3dddfac2153e381e859d
DmitryScaletta <dmitryscaletta@mail.ru>
2020-09-30 22:16:48 +0300

    feat: add fancyCount function

---
+++ src/utils/fancyCount.ts
@@ -0,0 +1,19 @@
+// https:// stackoverflow.com/a/54369738/4687416
+
+const fancyCount = (text: string) => {
+  const joiner = '\u{200D}';
+  const split = text.split(joiner);
+  let count = 0;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const s of split) {
+    // removing the variation selectors
+    const num = Array.from(s.split(/[\ufe00-\ufe0f]/).join('')).length;
+    count += num;
+  }
+
+  // assuming the joiners are used appropriately
+  return count / split.length;
+};
+
+export default fancyCount;
