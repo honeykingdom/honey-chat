ac92bb3a54af8bc07f0f868116b7bb6a3cf1cea6
DmitryScaletta <dmitryscaletta@mail.ru>
2022-04-13 14:51:29 +0300

    🔥 Remove old utils

--- src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-import type { SerializedError } from '@reduxjs/toolkit';
-
-export type FetchStatus = 'idle' | 'loading' | 'success' | 'error';
-
-export type FetchResult<T> = {
-  status: FetchStatus;
-  error: SerializedError;
-  items: T;
-};
