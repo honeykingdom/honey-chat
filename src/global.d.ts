4e72ad03d3aaee5b604eff7c9183868f1901f7bc
DmitryScaletta <dmitryscaletta@mail.ru>
2022-04-24 20:36:33 +0300

    🏷️ Update global types

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,15 +3,7 @@ declare module '*.ogg' {
   export default url;
 }
 
-declare module 'features/emotes/emojisMap.json' {
-  type Emoji = {
-    short: string;
-    keywords: string[];
-    char: string;
-    unified: string;
-  };
-
-  const data: Record<string, Emoji>;
-
-  export default data;
+declare module '*.svg' {
+  const url: string;
+  export default url;
 }
