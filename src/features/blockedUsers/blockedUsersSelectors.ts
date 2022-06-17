9e88d977a2b6d4b27c627b7d2ff43c50c019ce34
DmitryScaletta <dmitryscaletta@mail.ru>
2022-06-17 23:32:10 +0300

    🔥 Remove blocked users slice

--- src/features/blockedUsers/blockedUsersSelectors.ts
+++
@@ -1,8 +0,0 @@
-import type { RootState } from 'app/rootReducer';
-
-export const isBlockedUsersLoadedSelector = (state: RootState) =>
-  state.blockedUsers.status !== 'idle' &&
-  state.blockedUsers.status !== 'loading';
-
-export const blockedUsersSelector = (state: RootState) =>
-  state.blockedUsers.items;
