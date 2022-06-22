8f92d45a818a21af35b364f770fabede6374354f
DmitryScaletta <dmitryscaletta@mail.ru>
2022-06-22 19:54:05 +0300

    🩹 Export all apis

---
+++ src/features/api/index.ts
@@ -0,0 +1,16 @@
+export * from './twitch/twitchApiSlice';
+export * from './bttv/bttvApiSlice';
+export * from './ffz/ffzApiSlice';
+export * from './stv/stvApiSlice';
+export * from './chatterino/chatterinoApiSlice';
+export * from './recentMessages/recentMessagesApiSlice';
+export * from './youtube/youtubeApiSlice';
+
+export * from './twitch/twitchApiTypes';
+export * from './bttv/bttvApiTypes';
+export * from './ffz/ffzApiTypes';
+export * from './stv/stvApiTypes';
+export * from './chatterino/chatterinoApiTypes';
+export * from './youtube/youtubeApiTypes';
+
+export * from './types';
