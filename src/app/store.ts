70a0459dd0accb3ef7c1d31af6311cbdba9b303e
DmitryScaletta <dmitryscaletta@mail.ru>
2022-07-21 09:04:48 +0300

    ✨ Update app store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,22 +1,57 @@
-import { configureStore, Action } from '@reduxjs/toolkit';
-import type { ThunkAction } from 'redux-thunk';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+// import { setupListeners } from '@reduxjs/toolkit/query';
+import auth from 'features/auth/authSlice';
+import chat from 'features/chat/chatSlice';
+import options from 'features/options/optionsSlice';
+import { twitchApi } from 'features/api/twitch/twitchApiSlice';
+import { bttvApi } from 'features/api/bttv/bttvApiSlice';
+import { ffzApi } from 'features/api/ffz/ffzApiSlice';
+import { stvApi } from 'features/api/stv/stvApiSlice';
+import { chatterinoApi } from 'features/api/chatterino/chatterinoApiSlice';
+import { recentMessagesApi } from 'features/api/recentMessages/recentMessagesApiSlice';
+import { youtubeApi } from 'features/api/youtube/youtubeApiSlice';
 
-import rootReducer, { RootState } from 'app/rootReducer';
-
-const store = configureStore({
-  reducer: rootReducer,
-});
-
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./rootReducer', () => {
-    // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
-    const newRootReducer = require('./rootReducer').default;
-    store.replaceReducer(newRootReducer);
+export const makeStore = () =>
+  configureStore({
+    reducer: {
+      auth,
+      chat,
+      options,
+      [twitchApi.reducerPath]: twitchApi.reducer,
+      [bttvApi.reducerPath]: bttvApi.reducer,
+      [ffzApi.reducerPath]: ffzApi.reducer,
+      [stvApi.reducerPath]: stvApi.reducer,
+      [chatterinoApi.reducerPath]: chatterinoApi.reducer,
+      [recentMessagesApi.reducerPath]: recentMessagesApi.reducer,
+      [youtubeApi.reducerPath]: youtubeApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => [
+      ...getDefaultMiddleware({ serializableCheck: false }),
+      twitchApi.middleware,
+      bttvApi.middleware,
+      ffzApi.middleware,
+      stvApi.middleware,
+      chatterinoApi.middleware,
+      recentMessagesApi.middleware,
+      youtubeApi.middleware,
+    ],
   });
-}
+
+const store = makeStore();
+
+// https://redux-toolkit.js.org/rtk-query/usage/cache-behavior#re-fetching-on-window-focus-with-refetchonfocus
+// enable listener behavior for the store
+// setupListeners(store.dispatch)
+
+export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
 
-export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
 
 export default store;
