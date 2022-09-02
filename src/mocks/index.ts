53cf797ba9ea374f767df5da8451dcfa469c6a7f
DmitryScaletta <dmitryscaletta@mail.ru>
2022-09-02 07:42:53 +0300

    🩹 Parse api responses in mocks

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,42 +1,114 @@
-import * as R from 'ramda';
-
-import * as api from 'api';
-import twitchEmotes from 'mocks/twitchEmotes.json';
-import bttvGlobal from 'mocks/bttvGlobalEmotes.json';
-import bttvChannel from 'mocks/bttvChannelEmotes.json';
-import ffzGlobal from 'mocks/ffzGlobalEmotes.json';
-import ffzChannel from 'mocks/ffzChannelEmotes.json';
+import type { AllEmotes } from 'features/emotes';
+import { parseTwitchEmotes } from 'features/api/twitch/twitchApiParseResponses';
 import {
-  parseTwitchEmotes,
-  parseBttvGlobalEmotes,
   parseBttvChannelEmotes,
-  parseFfzGlobalEmotes,
+  parseBttvGlobalEmotes,
+} from 'features/api/bttv/bttvApiParseResponses';
+import {
   parseFfzChannelEmotes,
-} from 'features/chat/utils/parseApiResponse';
-import findEmote from 'features/emotes/utils/findEmote';
+  parseFfzEmoji,
+  parseFfzGlobalEmotes,
+} from 'features/api/ffz/ffzApiParseResponses';
+import { parseStvEmotes } from 'features/api/stv/stvApiParseResponses';
+import {
+  MessagePartBttvEmote,
+  MessagePartEmoji,
+  MessagePartFfzEmote,
+  MessagePartLink,
+  MessagePartMention,
+  MessagePartStvEmote,
+  MessagePartText,
+  MessagePartTwitchEmote,
+  MessagePartType,
+} from 'features/messages';
 
-export const emotes = {
-  twitchGlobal: R.pipe(
-    parseTwitchEmotes,
-    R.pick(['0']),
-  )((twitchEmotes as unknown) as api.TwitchEmotesResponse[]),
-  twitchUser: R.pipe(
-    parseTwitchEmotes,
-    R.omit(['0']),
-  )((twitchEmotes as unknown) as api.TwitchEmotesResponse[]),
-  bttvGlobal: parseBttvGlobalEmotes(bttvGlobal as api.BttvGlobalEmotesResponse),
-  bttvChannel: parseBttvChannelEmotes(
-    bttvChannel as api.BttvChannelEmotesResponse,
-  ),
-  ffzGlobal: parseFfzGlobalEmotes(ffzGlobal),
-  ffzChannel: parseFfzChannelEmotes(ffzChannel),
+import twitchEmotes from './twitchEmotes.json';
+import twitchBadgesGlobal from './twitchBadgesGlobal.json';
+import twitchBadgesChannel from './twitchBadgesChannel.json';
+import twitchBlockedUsers from './twitchBlockedUsers.json';
+import twitchClip from './twitchClip.json';
+import twitchVideo from './twitchVideo.json';
+import twitchUser from './twitchUser.json';
+import bttvGlobalEmotes from './bttvGlobalEmotes.json';
+import bttvChannelEmotes from './bttvChannelEmotes.json';
+import bttvGlobalBadges from './bttvGlobalBadges.json';
+import ffzGlobalEmotes from './ffzGlobalEmotes.json';
+import ffzChannelEmotes from './ffzChannelEmotes.json';
+import ffzEmoji from './ffzEmoji.json';
+import ffzGlobalBadges from './ffzGlobalBadges.json';
+import ffzApGlobalBadges from './ffzApGlobalBadges.json';
+import stvGlobalEmotes from './stvGlobalEmotes.json';
+import stvChannelEmotes from './stvChannelEmotes.json';
+import stvCosmetics from './stvCosmetics.json';
+
+export const emotes: AllEmotes = {
+  twitch: parseTwitchEmotes(twitchEmotes as any),
+  bttvGlobal: parseBttvGlobalEmotes(bttvGlobalEmotes as any),
+  bttvChannel: parseBttvChannelEmotes(bttvChannelEmotes as any),
+  ffzGlobal: parseFfzGlobalEmotes(ffzGlobalEmotes as any),
+  ffzChannel: parseFfzChannelEmotes(ffzChannelEmotes as any),
+  stvGlobal: parseStvEmotes(stvGlobalEmotes as any),
+  stvChannel: parseStvEmotes(stvChannelEmotes as any),
+  emoji: parseFfzEmoji(ffzEmoji as any),
 };
 
-export const findTwitchEmote = (name: string) =>
-  findEmote.twitch.byName(name, emotes);
+export const messagePartText = (content: string): MessagePartText => ({
+  type: MessagePartType.TEXT,
+  content,
+});
 
-export const findBttvEmote = (name: string) =>
-  findEmote.bttv.byName(name, emotes);
+export const messagePartMention = (
+  displayText: string,
+  recipient: string,
+): MessagePartMention => ({
+  type: MessagePartType.MENTION,
+  content: { displayText, recipient },
+});
 
-export const findFfzEmote = (name: string) =>
-  findEmote.ffz.byName(name, emotes);
+export const messagePartLink = (link: string): MessagePartLink => ({
+  type: MessagePartType.LINK,
+  content: { displayText: link, url: link },
+});
+
+export const messagePartTwitchEmote = (
+  name: string,
+): MessagePartTwitchEmote => {
+  const id = emotes.twitch?.names[name];
+
+  if (!id) throw new Error(`No twitch emote with name ${name}`);
+
+  return { type: MessagePartType.TWITCH_EMOTE, content: id };
+};
+
+export const messagePartBttvEmote = (name: string): MessagePartBttvEmote => {
+  const id = emotes.bttvGlobal?.names[name] || emotes.bttvChannel?.names[name];
+
+  if (!id) throw new Error(`No BTTV emote with name ${name}`);
+
+  return { type: MessagePartType.BTTV_EMOTE, content: id };
+};
+
+export const messagePartFfzEmote = (name: string): MessagePartFfzEmote => {
+  const id = emotes.ffzGlobal?.names[name] || emotes.ffzChannel?.names[name];
+
+  if (!id) throw new Error(`No FFZ emote with name ${name}`);
+
+  return { type: MessagePartType.FFZ_EMOTE, content: id };
+};
+
+export const messagePartStvEmote = (name: string): MessagePartStvEmote => {
+  const id = emotes.stvGlobal?.names[name] || emotes.stvChannel?.names[name];
+
+  if (!id) throw new Error(`No 7TV emote with name ${name}`);
+
+  return { type: MessagePartType.STV_EMOTE, content: id };
+};
+
+// https://twemoji.maxcdn.com/v/latest/72x72/1f47f.png
+export const messagePartEmoji = (emoji: string): MessagePartEmoji => {
+  const id = emotes.emoji?.names[emoji];
+
+  if (!id) throw new Error(`No emoji with id ${id}`);
+
+  return { type: MessagePartType.EMOJI, content: id };
+};
