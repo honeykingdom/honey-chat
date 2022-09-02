2ce981cfb57e6797bf70107819987a0ef87c59b3
DmitryScaletta <dmitryscaletta@mail.ru>
2022-09-02 16:45:25 +0300

    📈 Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -68,19 +68,19 @@ const App = ({ Component, pageProps }: AppProps) => (
   </CacheProvider>
 );
 
-// export const reportWebVitals = ({
-//   id,
-//   name,
-//   label,
-//   value,
-// }: NextWebVitalsMetric) => {
-//   window.gtag('event', name, {
-//     event_category:
-//       label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-//     value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-//     event_label: id, // id unique to current page load
-//     non_interaction: true, // avoids affecting bounce rate.
-//   });
-// };
+export const reportWebVitals = ({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) => {
+  window.gtag('event', name, {
+    event_category:
+      label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  });
+};
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-// const gaScript = `
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
-//   gtag('config', '${process.env.GA_TRACKING_ID}', { page_path: window.location.pathname });
-// `;
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+
+const gaScript = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });
+`;
 
 const Document = () => (
   <Html lang="ru">
@@ -31,11 +33,11 @@ const Document = () => (
       <meta name="apple-mobile-web-app-title" content="Honey Chat" />
       <meta name="application-name" content="Honey Chat" />
       <meta name="theme-color" content="#0e0e10" />
-      {/* <script
+      <script
         async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
-      <script dangerouslySetInnerHTML={{ __html: gaScript }} /> */}
+      <script dangerouslySetInnerHTML={{ __html: gaScript }} />
     </Head>
     <body>
       <Main />
