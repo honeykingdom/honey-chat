c8c43275cc8e44073b1c55ffddf8fc0b3f5f1a28
DmitryScaletta <dmitryscaletta@mail.ru>
2022-09-04 23:22:30 +0300

    🩹 Add history base url

--- src/features/api/recentMessages/recentMessagesApi.ts
+++ src/features/api/recentMessages/recentMessagesApi.ts
@@ -3,14 +3,16 @@ export type RecentMessagesResponse = {
   error: null;
 };
 
-const alternativeHistoryChannels = (
+const ALTERNATIVE_HISTORY_BASE_URL =
+  process.env.NEXT_PUBLIC_ALTERNATIVE_HISTORY_BASE_URL;
+const ALTERNATIVE_HISTORY_CHANNELS = (
   process.env.NEXT_PUBLIC_ALTERNATIVE_HISTORY_CHANNELS || ''
 ).split(';');
 
 const recentMessages = {
   get: (channelName: string): Promise<RecentMessagesResponse> => {
-    const url = alternativeHistoryChannels.includes(channelName)
-      ? `https://honeykingdom.herokuapp.com/api/v1/recent-messages/${channelName}`
+    const url = ALTERNATIVE_HISTORY_CHANNELS.includes(channelName)
+      ? `${ALTERNATIVE_HISTORY_BASE_URL}/api/v1/recent-messages/${channelName}`
       : `https://recent-messages.robotty.de/api/v2/recent-messages/${channelName}?clearchatToNotice=true`;
     return fetch(url).then((r) => r.json());
   },
