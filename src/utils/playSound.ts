5ae5efaed91ff1d87afba3fef5976d7416eaf095
DmitryScaletta <dmitryscaletta@mail.ru>
2022-09-14 19:22:54 +0300

    🩹 Lazy load sounds

--- src/utils/playSound.ts
+++ src/utils/playSound.ts
@@ -1,11 +1,18 @@
 import { Howl } from 'howler';
 
-const tink = new Howl({ src: ['/sounds/ts-tink.ogg'] });
-
-const SOUNDS = {
-  tink,
+const SOUNDS: Record<string, string | Howl> = {
+  tink: '/sounds/ts-tink.ogg',
 };
 
-const playSound = (name: keyof typeof SOUNDS) => SOUNDS[name].play();
+const playSound = (name: keyof typeof SOUNDS) => {
+  let sound = SOUNDS[name];
+
+  if (typeof sound === 'string') {
+    sound = new Howl({ src: [sound] });
+    SOUNDS[name] = sound;
+  }
+
+  return sound.play();
+};
 
 export default playSound;
