9a64f36691c7067e9e89eaa3ecc798ac54684707
DmitryScaletta <dmitryscaletta@mail.ru>
2024-03-14 17:00:01 +0300

    🩹 Change recent messages api url

--- src/features/api/recentMessages/recentMessagesApi.ts
+++ src/features/api/recentMessages/recentMessagesApi.ts
@@ -3,8 +3,6 @@ export type RecentMessagesResponse = {
   error: null;
 };
 
-const ALTERNATIVE_HISTORY_BASE_URL =
-  process.env.NEXT_PUBLIC_ALTERNATIVE_HISTORY_BASE_URL;
 const ALTERNATIVE_HISTORY_CHANNELS = (
   process.env.NEXT_PUBLIC_ALTERNATIVE_HISTORY_CHANNELS || ''
 ).split(';');
@@ -12,7 +10,7 @@ const ALTERNATIVE_HISTORY_CHANNELS = (
 const recentMessages = {
   get: (channelName: string): Promise<RecentMessagesResponse> => {
     const url = ALTERNATIVE_HISTORY_CHANNELS.includes(channelName)
-      ? `${ALTERNATIVE_HISTORY_BASE_URL}/api/v1/recent-messages/${channelName}`
+      ? `https://diverse-marillin-honeykingdom.koyeb.app/api/twitch/recent-messages?channel=${channelName}`
       : `https://recent-messages.robotty.de/api/v2/recent-messages/${channelName}?clearchatToNotice=true`;
     return fetch(url).then((r) => r.json());
   },
